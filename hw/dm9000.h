402080a9a36cf9e6c705894d34fe0bfd4f5e1112
Michel <michel.pollet@peavey-eu.com>
2009-02-23 12:58:55 +0000

    [DM9000] Imported Simtec files
    Imported DM9000 ethernet device from simtec's abandonned (?) project.

---
+++ hw/dm9000.h
@@ -0,0 +1,17 @@
+/* hw/dm9000.h
+ *
+ * DM9000 Ethernet interface
+ *
+ * Copyright Daniel Silverstone and Vincent Sanders
+ *
+ * This file is under the terms of the GNU General Public
+ * License Version 2
+ */
+
+#ifndef QEMU_HW_DM9000_H
+#define QEMU_HW_DM9000_H
+
+void dm9000_init(NICInfo *nd, target_phys_addr_t base_addr, uint32_t addr_offset, 
+                 uint32_t data_offset, qemu_irq irq);
+
+#endif
